174761b78a9dbcc455b926f8acfb512ba709325d
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-07-27 19:50:02 +0200

    16.-21.

---
+++ FPC/Assets/FPController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPController : MonoBehaviour
+{
+    public GameObject cam;
+    float speed = 0.1f;
+    float Xsensitivity = 4;
+    float Ysensitivity = 4;
+    float MinimumX = -90;
+    float MaximumX = 90;
+
+    bool cursorIsLocked = true;
+    bool lockCursor = true;
+
+    Rigidbody rb;
+    CapsuleCollider capsule;
+
+    Quaternion cameraRot;
+    Quaternion characterRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        capsule = this.GetComponent<CapsuleCollider>();
+
+        cameraRot = cam.transform.localRotation;
+        characterRot = this.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float yRot = Input.GetAxis("Mouse X") * Ysensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * Xsensitivity;
+
+        cameraRot *= Quaternion.Euler(-xRot, 0, 0);
+        characterRot *= Quaternion.Euler(0, yRot, 0);
+
+        cameraRot = ClampRotationAroundXAxis(cameraRot);
+
+        this.transform.localRotation = characterRot;
+        cam.transform.localRotation = cameraRot;
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+            rb.AddForce(0, 300, 0);
+
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        transform.position += cam.transform.forward * z + cam.transform.right * x;//new Vector3(x * speed, 0, z * speed);
+
+        UpdateCursorLock();
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.y /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hitInfo;
+        if (Physics.SphereCast(transform.position, capsule.radius, Vector3.down, out hitInfo,
+                (capsule.height / 2f) - capsule.radius + 0.1f))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock()
+    {
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    public void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            cursorIsLocked = false;
+        else if (Input.GetMouseButtonUp(0))
+            cursorIsLocked = true;
+
+        if (cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+}
