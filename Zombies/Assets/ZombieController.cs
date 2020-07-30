897d61fca1ba5c8c37a154ae6aa94debbd95fe6e
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-07-31 01:04:44 +0200

    47. Creating Animated Zombies

---
+++ Zombies/Assets/ZombieController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+            anim.SetBool("isWalking", false);
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+            anim.SetBool("isRunning", false);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+            anim.SetBool("isAttacking", false);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isDead", true);
+        }
+
+    }
+}
