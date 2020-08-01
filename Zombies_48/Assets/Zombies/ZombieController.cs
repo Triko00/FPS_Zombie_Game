28a192aceeb1a00c01704706d4eba6154ab6d2cd
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-01 19:03:09 +0200

    51. Finite State Machines Part 1

--- Zombies_48/Assets/Zombies/ZombieController.cs
+++ Zombies_48/Assets/Zombies/ZombieController.cs
@@ -9,56 +9,59 @@ public class ZombieController : MonoBehaviour
     Animator anim;
     NavMeshAgent agent;
 
+    enum STATE { IDLE, WANDER, ATTACK, CHASE, DEAD };
+    STATE state = STATE.IDLE;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = this.GetComponent<Animator>();
         agent = this.GetComponent<NavMeshAgent>();
-
-        anim.SetBool("isWalking", true);
+    }
+    
+    void TurnOffTriggers()
+    {
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isAttacking", false);
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isDead", false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        agent.SetDestination(target.transform.position);
-        if (agent.remainingDistance > agent.stoppingDistance)
+        switch (state)
         {
-            anim.SetBool("isWalking", true);
-            anim.SetBool("isAttacking", false);
-        }
-        else
-        {
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isAttacking", true);
-        }
+            case STATE.IDLE:
+                state = STATE.WANDER;
+                break;
+            case STATE.WANDER:
+                if (!agent.hasPath) {
+                    float newX = this.transform.position.x + Random.Range(-5, 5);
+                    float newZ = this.transform.position.z + Random.Range(-5, 5);
+                    float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ));
+                    Vector3 dest = new Vector3(newX, newY, newZ);
+                    agent.SetDestination(dest);
+                    agent.stoppingDistance = 0;
+                    TurnOffTriggers();
+                    anim.SetBool("isWalking", true);
 
-        /*if (Input.GetKey(KeyCode.W))
-        {
-            anim.SetBool("isWalking", true);
+                    //  debug
+                    //  if (this.transform.name == "jill")
+                    //  {
+                    //      Vector3 agentDest = agent.destination;
+                    //      Debug.Log("name = " + this.transform.name + " newX = " + newX + " newY = " + newY + " newZ = " + newZ);
+                    //      Debug.Log("name = " + this.transform.name + " agentDestX = " + agentDest.x + " agentDestY = " + agentDest.y + " agentDestZ = " + agentDest.z);
+                    //  }
+                }
+                break;
+            case STATE.CHASE:
+                break;
+            case STATE.ATTACK:
+                break;
+            case STATE.DEAD:
+                break;
         }
-        else
-            anim.SetBool("isWalking", false);
-
-        if (Input.GetKey(KeyCode.R))
-        {
-            anim.SetBool("isRunning", true);
-        }
-        else
-            anim.SetBool("isRunning", false);
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            anim.SetBool("isAttacking", true);
-        }
-        else
-            anim.SetBool("isAttacking", false);
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            anim.SetBool("isDead", true);
-        }*/ 
-
     }
 }
