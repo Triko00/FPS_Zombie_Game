e2e1db53faa8ebefe10c7fd1873dcefc5a85b295
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-02 16:15:12 +0200

    52. Finite State Machines Part 2

--- Zombies_48/Assets/Zombies/ZombieController.cs
+++ Zombies_48/Assets/Zombies/ZombieController.cs
@@ -27,6 +27,28 @@ public class ZombieController : MonoBehaviour
         anim.SetBool("isDead", false);
     }
 
+    float DistanceToPlayer()
+    {
+        return Vector3.Distance(target.transform.position, this.transform.position);
+    }
+
+    bool CanSeePlayer()
+    {
+        /*if (this.transform.name == "zombiegirl")
+            Debug.Log("Name = " + this.transform.name + " DistanceToPlayer = " + DistanceToPlayer());*/
+
+        if (DistanceToPlayer() < 10)
+            return true;
+        return false;
+    }
+
+    bool ForgetPlayer()
+    {
+        if (DistanceToPlayer() > 20)
+            return true;
+        return false;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -34,10 +56,13 @@ public class ZombieController : MonoBehaviour
         switch (state)
         {
             case STATE.IDLE:
-                state = STATE.WANDER;
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else
+                    state = STATE.WANDER;
                 break;
             case STATE.WANDER:
-                if (!agent.hasPath) {
+                if (!agent.hasPath)
+                {
                     float newX = this.transform.position.x + Random.Range(-5, 5);
                     float newZ = this.transform.position.z + Random.Range(-5, 5);
                     float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ));
@@ -47,18 +72,42 @@ public class ZombieController : MonoBehaviour
                     TurnOffTriggers();
                     anim.SetBool("isWalking", true);
 
-                    //  debug
-                    //  if (this.transform.name == "jill")
-                    //  {
-                    //      Vector3 agentDest = agent.destination;
-                    //      Debug.Log("name = " + this.transform.name + " newX = " + newX + " newY = " + newY + " newZ = " + newZ);
-                    //      Debug.Log("name = " + this.transform.name + " agentDestX = " + agentDest.x + " agentDestY = " + agentDest.y + " agentDestZ = " + agentDest.z);
-                    //  }
+                    /* debug
+                    if (this.transform.name == "jill")
+                    {
+                        Vector3 agentDest = agent.destination;
+                        Debug.Log("name = " + this.transform.name + " newX = " + newX + " newY = " + newY + " newZ = " + newZ);
+                        Debug.Log("name = " + this.transform.name + " agentDestX = " + agentDest.x + " agentDestY = " + agentDest.y + " agentDestZ = " + agentDest.z);
+                    }*/
                 }
+
+                if (CanSeePlayer()) state = STATE.CHASE;
+
                 break;
             case STATE.CHASE:
+                agent.SetDestination(target.transform.position);
+                agent.stoppingDistance = 5;
+                TurnOffTriggers();
+                anim.SetBool("isRunning", true);
+
+                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+                {
+                    state = STATE.ATTACK;
+                }
+
+                if (ForgetPlayer())
+                {
+                    state = STATE.WANDER;
+                    agent.ResetPath();
+                }
+                
                 break;
             case STATE.ATTACK:
+                TurnOffTriggers();
+                anim.SetBool("isAttacking", true);
+                this.transform.LookAt(target.transform.position);
+                if (DistanceToPlayer() > agent.stoppingDistance)
+                    state = STATE.CHASE;
                 break;
             case STATE.DEAD:
                 break;
