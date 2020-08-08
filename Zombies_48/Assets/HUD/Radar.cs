3e88cfcadc6380bb9c078855bf5d4010518cea1b
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-08-08 14:16:21 +0200

    73. Building a Zombie Radar Part 1

---
+++ Zombies_48/Assets/HUD/Radar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadarObject
+{
+    public Image icon { get; set; }
+    public GameObject owner { get; set; }
+}
+
+public class Radar : MonoBehaviour
+{
+    public Transform playerPos;
+    public float mapScale = 2.0f;
+
+    public static List<RadarObject> radObjects = new List<RadarObject>();
+
+    public static void RegisterRadarObject(GameObject o, Image i)
+    {
+        Image image = Instantiate(i);
+        radObjects.Add(new RadarObject() { owner = o, icon = image });
+    }
+
+    public static void RemoveRadarObject(GameObject o)
+    {
+        List<RadarObject> newList = new List<RadarObject>();
+        for(int i = 0; i <radObjects.Count; i++)
+        {
+            if (radObjects[i].owner == o)
+            {
+                Destroy(radObjects[i].icon);
+                continue;
+            }
+            else
+                newList.Add(radObjects[i]);
+        }
+
+        radObjects.RemoveRange(0, radObjects.Count);
+        radObjects.AddRange(newList);
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
